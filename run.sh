a6de4f2d2786bc9936bda989242a7c340f2bd182
Adam Kolar <adakolar@gmail.com>
2018-05-06 13:37:58 +0200

    keras vs pytoech init

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tensorboard --port 6006 --logdir /tensorboard_summaries/ &
+#tensorboard --port 6006 --logdir /tensorboard_summaries/ &
 ipython notebook --ip='*' --NotebookApp.token='' --allow-root --port 8000 /notebooks/
