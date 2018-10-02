fdd4047eb4683e71c72c81ce5fc5efe3980d7303
Adam Kolar <adakolar@gmail.com>
2018-10-02 15:20:35 +0200

    proper ip

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 tensorboard --port 6006 --logdir /tensorboard_summaries/ &
-ipython notebook --ip='*' --NotebookApp.token='' --allow-root --port 8000 /notebooks/
+ipython notebook --ip='0.0.0.0' --NotebookApp.token='' --allow-root --port 8000 /notebooks/
